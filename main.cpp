921cbfedf3a18cd384518945a0027b09189ef1f7
Tyler Orr <Tyler_Orr@baylor.edu>
2012-09-27 19:59:05 -0500

    Added main.cpp
    Started outlining classes in README

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Filename:		driver.cpp
+ * Created:		09.27.12
+ * Description:		This file prints "Hello, World!" to the screen
+ ******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello, World!" << endl;
+
+	return 0;
+}
